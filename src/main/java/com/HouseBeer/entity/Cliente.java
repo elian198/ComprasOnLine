d3ace56fedc36fe5cf8cb572b58477c4cab3c8a0
elian <elianpareja87@hotmail.com>
2024-05-27 12:31:40 -0300

    agrego pruebas unitarias en repo de usuario

--- src/main/java/com/HouseBeer/entity/Cliente.java
+++ src/main/java/com/HouseBeer/entity/Cliente.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCliente;
 
     @Column(name = "NOMBRE")
--- src/main/java/com/HouseBeer/entity/Zona.java
+++ src/main/java/com/HouseBeer/entity/Zona.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Zona {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String lugar;
